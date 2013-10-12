26c8773d04cdcd30d1888794afa8d4d4d187aece
James Young <james.young@turner.com>
2013-10-12 15:29:48 -0400

    supports message with and without color

--- log-notify.js
+++ log-notify.js
@@ -1,13 +1,18 @@
 'use strict';
 
+var debug = require('debug')('log-notify');
+
 module.exports = (function log() {
     var notifier;
 
     switch (process.platform) {
     case 'darwin':
+        debug('setting notifier to terminal-notifier');
+
         notifier = require('terminal-notifier');
         return function (message) {
-            notifier(message.stripColors, {
+            var cleanedMessage = typeof message.stripColors === 'string' ? message.stripColors : message;
+            notifier(cleanedMessage, {
                 title: 'Dust Compiler',
                 activate: 'com.apple.Terminal'
             });
@@ -16,9 +21,12 @@ module.exports = (function log() {
 
     case 'linux':
     case 'win32':
+        debug('setting notifier to growl');
+
         notifier = require('growl');
         return function (message) {
-            notifier(message.stripColors, { title: 'Dust Compiler' });
+            var cleanedMessage = typeof message.stripColors === 'string' ? message.stripColors : message;
+            notifier(cleanedMessage, { title: 'Dust Compiler' });
             console.log(message);
         };
 
