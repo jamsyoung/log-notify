84ee8cf1fc0f7f3b84cab001a11bb24535f3d00b
James Young <james.young@turner.com>
2013-10-12 16:05:22 -0400

    more unit tests

--- test/mocha/log-notify.test.js
+++ test/mocha/log-notify.test.js
@@ -7,4 +7,15 @@ describe('log-notify', function () {
     it('should should return a function', function () {
         log.should.be.a('function');
     });
+
+    it('should work with a basic string', function () {
+        // this isn't a 'true' test since grunt itself installs 'color', which provides
+        // the 'stripColors' method on a string object that log-notify tests for.
+        log('hello world');
+    });
+
+    it('should work with a string with color', function () {
+        // This means it can successfully strip off the colors before sending to the notifier
+        log('hello world with color'.rainbow);
+    });
 });
