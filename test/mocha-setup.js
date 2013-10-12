53e53a80243b3d10279fd17f79f80740fc30ada7
James Young <james.young@turner.com>
2013-10-12 16:04:55 -0400

    removed blanket dependency

--- test/mocha-setup.js
+++ test/mocha-setup.js
@@ -12,21 +12,3 @@ global.should = chai.should();
 
 // include stack track on test failure
 chai.Assertion.includeStack = false;
-
-// needed for blanket instrumentation with mocha
-// require('blanket')({
-//     'data-cover-only': ['cli-wrapper.js', 'process.js'],
-//     'data-cover-never': ['node_modules', 'test']
-// });
-
-
-// ---------------------
-
-// require('blanket')({
-//     'data-cover-only': [ 'src/' ],
-//     'data-cover-never': ['node_modules', 'test']
-// });
-// require('blanket')({
-    // Only files that match the pattern will be instrumented
-//     pattern: '/src/'
-// });
